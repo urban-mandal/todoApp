88ef1b7153b893abd7d693cc4746aecf9441230d
Strela196 <119959434+Strela196@users.noreply.github.com>
2024-08-12 20:01:00 +0200

    design homepage changes

--- static/homepage.js
+++ static/homepage.js
@@ -1,7 +1,12 @@
 const sidebarButton = document.getElementById("sidebar");
 const sidebar = document.getElementById("sideBarDiv");
+const taskDiv = document.getElementById("tasks");
+const profileDiv = document.getElementById("profile");
+const profileTools = document.getElementById("profileTools");
+const closeDivButton = document.getElementById("closeDiv");
 
 sidebarButton.onclick = turnAround;
+closeDivButton.onclick = closeSideBars;
 
 function turnAround() {
   if (
@@ -18,3 +23,27 @@ function turnAround() {
     sidebar.style.display = "flex";
   }
 }
+
+taskDiv.addEventListener("click", function () {
+  closeSideBars();
+});
+
+function closeSideBars() {
+  if (sidebar.style.display === "flex") {
+    sidebar.style.display = "none";
+    sidebarButton.style.transform = "rotate(0deg)";
+    taskDiv.style.marginLeft = "0%";
+    taskDiv.style.width = "100%";
+  }
+  if (profileTools.style.display === "flex") {
+    profileTools.style.display = "none";
+  }
+}
+
+profileDiv.addEventListener("click", function () {
+  if (profileTools.style.display === "flex") {
+    profileTools.style.display = "none";
+  } else {
+    profileTools.style.display = "flex";
+  }
+});
